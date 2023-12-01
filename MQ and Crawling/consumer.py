627244849cd24ac12c6f17589d5f7ca4695be431
Akshit7103 <125750297+Akshit7103@users.noreply.github.com>
2023-12-01 23:49:52 +0530

    Add files via upload

--- MQ and Crawling/consumer.py
+++ MQ and Crawling/consumer.py
@@ -3,57 +3,95 @@ import requests
 from bs4 import BeautifulSoup
 import json
 from urllib.parse import urljoin
+import pandas as pd
+import mysql.connector
+import threading
 
-def crawl(url, depth, keywords, hits, hit_counts, visited_urls):
+def get_keywords_from_db():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',  # Replace with your MySQL username
+            password='82461937Cr7@',  # Replace with your MySQL password
+            database='web_crawler'
+        )
+        cursor = connection.cursor()
+        cursor.execute("SELECT keyword FROM keywords")
+        keywords = [item[0] for item in cursor.fetchall()]
+        cursor.close()
+        connection.close()
+        return keywords
+    except mysql.connector.Error as err:
+        print("Error connecting to MySQL:", err)
+        return []
+
+def crawl(url, depth, keywords, hits, visited_urls, lock):
     if depth == 0 or url in visited_urls:
         return
 
-    visited_urls.add(url)
+    with lock:
+        if url in visited_urls:
+            return
+        visited_urls.add(url)
 
     try:
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
-
         text = soup.get_text().lower()
+
         for keyword in keywords:
             lower_keyword = keyword.lower()
             count = text.count(lower_keyword)
             if count > 0:
-                print(f"Keyword '{keyword}' found {count} times at {url}")
-                hits.append({'keyword': keyword, 'url': url, 'count': count})
-                hit_counts[keyword] += count
+                with lock:
+                    print(f"Keyword '{keyword}' found {count} times at {url}")
+                    hits.append({'Keyword': keyword, 'URL': url, 'Count': count})
 
         for link in soup.find_all('a'):
             href = link.get('href')
             if href and not href.startswith('#'):
                 absolute_url = urljoin(url, href)
-                crawl(absolute_url, depth - 1, keywords, hits, hit_counts, visited_urls)
+                if absolute_url not in visited_urls:
+                    threading.Thread(target=crawl, args=(absolute_url, depth - 1, keywords, hits, visited_urls, lock)).start()
 
     except requests.RequestException as e:
-        print(f"Failed to crawl {url}: {str(e)}")
+        with lock:
+            print(f"Failed to crawl {url}: {str(e)}")
 
 def on_request(ch, method, properties, body):
     data = json.loads(body)
+    print(" [x] Received crawl request for", data['url'])
+
+    keywords = get_keywords_from_db()
     hits = []
-    hit_counts = {keyword.lower(): 0 for keyword in data['keywords']}
     visited_urls = set()
+    lock = threading.Lock()
 
-    crawl(data['url'], data['depth'], data['keywords'], hits, hit_counts, visited_urls)
+    crawl(data['url'], data['depth'], keywords, hits, visited_urls, lock)
 
-    with open('keyword_hits.txt', 'a') as file:
-        for hit in hits:
-            file.write(f"Keyword: {hit['keyword']} found {hit['count']} times at URL: {hit['url']}\n")
-        file.write(f"\nTotal keyword hits:\n")
-        for keyword, count in hit_counts.items():
-            file.write(f"{keyword}: {count}\n")
+    # Wait for all threads to finish
+    main_thread = threading.current_thread()
+    for t in threading.enumerate():
+        if t is main_thread:
+            continue
+        t.join()
 
+    # Save hits to an Excel file
+    if hits:
+        df = pd.DataFrame(hits)
+        df.to_excel('keyword_hits.xlsx', index=False)
+
+    print("Crawling completed for request:", data['url'])
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
+# Establish connection with RabbitMQ server
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
+# Declare the queue as durable
 channel.queue_declare(queue='crawl_requests', durable=True)
 
+# Set up subscription on the queue
 channel.basic_consume(queue='crawl_requests', on_message_callback=on_request)
 
 print(' [*] Waiting for crawl requests. To exit press CTRL+C')
--- MQ and Crawling/producer.py
+++ MQ and Crawling/producer.py
@@ -1,23 +1,29 @@
 import pika
 import json
 
+# Establish connection with RabbitMQ server
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
+# Declare a queue as durable
 channel.queue_declare(queue='crawl_requests', durable=True)
 
+# Input details for the crawl
 url = input("Enter URL to crawl: ")
 depth = int(input("Enter depth of crawl: "))
-keywords = input("Enter keywords separated by commas: ").split(',')
 
-message = json.dumps({"url": url, "depth": depth, "keywords": keywords})
+# Create a message
+# Keywords are not included as they are fetched from the database in the consumer script
+message = json.dumps({"url": url, "depth": depth})
 
+# Publish the message
 channel.basic_publish(exchange='',
                       routing_key='crawl_requests',
                       body=message,
                       properties=pika.BasicProperties(
-                         delivery_mode=2,
+                         delivery_mode=2,  # make message persistent
                       ))
 print(" [x] Sent crawl request")
 
+# Close the connection
 connection.close()
